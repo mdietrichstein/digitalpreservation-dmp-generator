3e5708bde81167b00e87fb8c7e94965b2e1e2c9d
Marc Dietrichstein <marc@dietrichstein.net>
2018-04-22 13:02:11 +0200

    Start ui implementation

--- src/main/java/at/ac/tuwien/digital_preservation_2018/group2_4/fair/dmpgenerator/orcid/OrcidController.java
+++ src/main/java/at/ac/tuwien/digital_preservation_2018/group2_4/fair/dmpgenerator/orcid/OrcidController.java
@@ -23,20 +23,20 @@ public class OrcidController {
 
 	// defs
 	private static final Logger log = LoggerFactory.getLogger(OrcidController.class.getCanonicalName());
-	
+
 	// dependencies
 	private final OrcidRESTApi orcidRESTApi;
-	
+
 	// initialization
 	@Autowired
 	public OrcidController(OrcidRESTApi orcidRESTApi) {
 		this.orcidRESTApi = orcidRESTApi;
 	}
-	
+
 	// web
 	@RequestMapping(value="/search", method=RequestMethod.GET)
 	public OrcidSearchResponse search(@RequestParam("q") String query) {
-		
+
 		final JsonNode jsonResponse;
 		try {
 			jsonResponse = orcidRESTApi.search(query).execute().body();
@@ -44,20 +44,20 @@ public class OrcidController {
 			log.error("Error while fetching orcid record", e);
 			throw new RuntimeException(e);
 		}
-		
+
 		JsonNode jsonResults = jsonResponse.path("result");
-		
+
 		List<OrcidSearchResult> results = new ArrayList<>();
 		for(int i=0; i<jsonResults.size(); i++) {
-			results.add(convertOrcidSearchResult(jsonResults.get(0)));
+			results.add(convertOrcidSearchResult(jsonResults.get(i)));
 		}
-		
+
 		return new OrcidSearchResponse(results);
 	}
-	
+
 	@RequestMapping(value="/profile/{orcidId}", method=RequestMethod.GET)
 	public OrcidRecordResponse profile(@PathVariable("orcidId") String orcidId) {
-		
+
 		final JsonNode jsonResponse;
 		try {
 			jsonResponse = orcidRESTApi.record(orcidId).execute().body();
@@ -65,10 +65,10 @@ public class OrcidController {
 			log.error("Error while fetching orcid record", e);
 			throw new RuntimeException(e);
 		}
-		
+
 		return convertRecordResponse(jsonResponse);
 	}
-	
+
 	// private helpers
 	public static OrcidSearchResult convertOrcidSearchResult(JsonNode root) {
 		return new OrcidSearchResult(
@@ -76,38 +76,38 @@ public class OrcidController {
 					root.path("orcid-identifier").path("path").textValue(),
 					root.path("orcid-identifier").path("host").textValue());
 	}
-	
+
 	public static OrcidRecordResponse convertRecordResponse(JsonNode root) {
 		String givenName = root.path("person").path("name").path("given-names").path("value").textValue();
 		String familyName = root.path("person").path("name").path("family-name").path("value").textValue();
 		String email = root.path("person").path("emails").path("email").path(0).path("email").textValue();
-		
+
 		JsonNode work = root.path("activities-summary").path("works").path("group").path(0).path("work-summary").path(0);
 		String title = work.path("title").path("title").path("value").textValue();
 		String subtitle = work.path("title").path("subtitle").path("value").textValue();
-		
+
 		JsonNode externalId = work.path("external-ids").path("external-id").path(0);
 		String idType = externalId.path("external-id-type").textValue();
 		String id = externalId.path("external-id-value").textValue();
 		String idUrl = externalId.path("external-id-url").path("value").textValue();
-		
+
 		String publicationYear = work.path("publication-date").path("year").path("value").textValue();
 		String publicationMonth = work.path("publication-date").path("month").path("value").textValue();
 		String publicationDay = work.path("publication-date").path("day").path("value").textValue();
-		
+
 		return new OrcidRecordResponse(
 				givenName, familyName, email,
 				new OrcidRecordResponse.OrcidRecordProject(
 						title, subtitle, id, idType, idUrl,
 						publicationYear, publicationMonth, publicationDay)
 				);
-		
+
 	}
-	
+
 	// responses
 	public static class OrcidSearchResponse {
 		public final List<OrcidSearchResult> results;
-		
+
 		public OrcidSearchResponse(List<OrcidSearchResult> results) {
 			this.results = results;
 		}
@@ -116,7 +116,7 @@ public class OrcidController {
 			public final String uri;
 			public final String path;
 			public final String host;
-			
+
 			public OrcidSearchResult(String uri, String path, String host) {
 				this.uri = uri;
 				this.path = path;
@@ -130,7 +130,7 @@ public class OrcidController {
 		public final String familyName;
 		public final String emailAddress;
 		public final OrcidRecordProject project;
-		
+
 		public OrcidRecordResponse(String givenName, String familyName, String emailAddress, OrcidRecordProject project) {
 			this.givenName = givenName;
 			this.familyName = familyName;
@@ -141,15 +141,15 @@ public class OrcidController {
 		public static class OrcidRecordProject {
 			public final String title;
 			public final String subtitle;
-			
+
 			public final String id;
 			public final String idType;
 			public final String idUrl;
-			
+
 			public final String publicationYear;
 			public final String publicationMonth;
 			public final String publicationDay;
-			
+
 			public OrcidRecordProject(String title, String subtitle, String id, String idType, String idUrl,
 					String publicationYear, String publicationMonth, String publicationDay) {
 				this.title = title;
---
+++ src/main/resources/static/javascript/main.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+    setupUI();
+});
+
+
+// State / DAta
+var state = {
+    author: null
+};
+
+function setAuthor(author) {
+    state.author = author;
+    $('#debugAuthorProfile').text(JSON.stringify(author));
+}
+
+
+// API
+
+function loadOrcidProfile(orcidId) {
+    $.ajax({
+        url: 'http://localhost:8080/orcid/profile/' + orcidId
+    }).then(function(data) {
+        setAuthor(data);
+    });
+}
+
+// UI
+
+function setAuthorProfile(profile) {
+
+}
+
+function setupUI() {
+    setupAuthorSearchUI();
+}
+
+function setupAuthorSearchUI() {
+    var orcidResults = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        // prefetch: '../data/films/post_1960.json',
+        remote: {
+            url: 'http://localhost:8080/orcid/search?q=%QUERY',
+            wildcard: '%QUERY',
+            filter: function (data) {
+                return data.results;
+            }
+        }
+    });
+
+    $('#author_search').typeahead(
+        {
+            minLength: 3,
+
+        },
+        {
+            name: 'orcid-results',
+            display: 'value',
+            source: orcidResults,
+            templates: {
+                empty: "No orcid user found",
+                suggestion: function(data) {
+                    return '<p>' + data.path + '</p>';
+                }
+            }
+        }).on('typeahead:selected', function (obj, data) {
+            loadOrcidProfile(data.path);
+        });
+}
